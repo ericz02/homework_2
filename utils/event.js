264c87015dc81898e3dc54cd57abaee1a6fe10c8
ericz02 <ezheng0720@gmail.com>
2023-02-01 09:47:45 -0500

    Adds multiply

---
+++ utils/event.js
@@ -0,0 +1,26 @@
+import {add, multiply} from './math'
+
+const eventHandler = () => {
+
+    const addButton = document.getElementById('add')
+    const multiplyButton = document.getElementById('multiply')
+    const number1 = document.getElementById('number-1')
+    const number2 = document.getElementById('number-2')
+    const result = document.getElementById('result')
+    
+    const updateDomWithAddedNumbers = () => {
+        result.innerHTML =
+        // result.innerHTML = parseInt(number1.value, 10) + parseInt(number2.value, 10)
+        add(number1.value, number2.value)
+    }
+
+    const updateDomWithMultipliedNumbers = () => {
+        result.innerHTML = 
+        multiply(number1.value, number2.value)
+    }
+
+    addButton.addEventListener('click', updateDomWithAddedNumbers)
+    multiplyButton.addEventListener('click', updateDomWithMultipliedNumbers)
+}
+
+export {eventHandler};
